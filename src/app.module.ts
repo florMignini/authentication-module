26428a2a178eb0b643ec157411a025565a392641
florMignini <flormignini29@gmail.com>
2023-07-03 21:22:22 -0300

    postgres:14.3 ---> postgres:14-bullseye

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    EmployeesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
