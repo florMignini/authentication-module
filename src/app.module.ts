60c059a34f81c30567c6d712ff4417b8a2654c14
Florencia Mignini <flormignini@Mac-mini-de-Florencia.local>
2023-08-19 16:35:32 -0300

    prisma integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { EmployeeModule } from './employee/employee.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [AuthModule, EmployeeModule]
+  imports: [AuthModule, EmployeeModule, PrismaModule]
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthService, AuthController } from './';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
     controllers: [AuthController],
